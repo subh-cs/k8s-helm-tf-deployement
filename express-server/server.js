03cd92178a3c8249cee03aae3e859d052243130b
Subhodip Roy <subhodiproy281@gmail.com>
2024-03-12 10:07:10 +0530

    first commit

---
+++ express-server/server.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+})
+
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
+})
